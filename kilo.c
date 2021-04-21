f8a0852e76fac80df0e08daa346234604f460ffc
Aayush Kashyap <kashyapaayush32@gmail.com>
2021-04-21 21:31:48 +0530

    started chapter 3

--- kilo.c
+++ kilo.c
@@ -6,6 +6,10 @@
 #include <stdio.h>
 #include <ctype.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /*** data ***/
 
 struct termios orig_termios;
@@ -14,6 +18,9 @@ struct termios orig_termios;
 
 void die(const char* s)
 {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+  
   perror(s);
   exit(1);
 }
@@ -50,20 +57,56 @@ void enableRawMode()
   }
 }
 
+char editorReadKey()
+{
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+  {
+    if(nread == -1 && errno != EAGAIN)
+    {
+      die('read');
+    }
+  }
+  return c;
+}
+
+/*** input ***/
+
+void editorProcessKeypress()
+{
+  char c = editorReadKey();
+  
+  switch (c)
+  {
+    case CTRL_KEY('q') : 
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
+      
+      exit(0);
+      break;
+  }
+}
+
+/*** output ***/ 
+
+void editorRefreshScreen()
+{
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
 /*** main loop ***/
 
 int main()
 {
   enableRawMode ();
-  while (1){
-    char c = '\0';
-    if(read (STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN){ die ("read"); }
-	  if(iscntrl(c))
-		  printf("%d\r\n", c);
-	  else
-		  printf("%d (%c) \r\n", c, c);
-		  
-		  if(c == 'q') break;
+  
+  while (1)
+  {
+    editorRefreshScreen();
+    editorProcessKeypress();
   }
+  
   return 0;
 }
